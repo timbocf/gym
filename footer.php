d022e6b18fca6e2453cc909548c1f3fbd900b9da
Tim Rodgers <timbocf@gmail.com>
2014-10-06 11:38:51 -0700

    Okay, so maybe NOW the footer update is committed..

--- footer.php
+++ footer.php
@@ -15,17 +15,17 @@
 	        <div id="footergymlogo"><img src="<?php bloginfo('template_directory') ?>/images/logo_gray.png" alt="Gym 101 Fitness" title="Gym 101 Fitness" /></div>
 	        <div id="menu2">
 	            <ul>
-	                <li><a href="index.php?page=home">Home</a></li>
-	                <li><a href="index.php?page=training">Personal Training/Sports Nutrition</a></li>
-	                <li><a href="index.php?page=class">Class Schedule</a></li>
+	                <li><a href="/home/">Home</a></li>
+	                <li><a href="/personal-training-sports-nutrition/">Personal Training/Sports Nutrition</a></li>
+	                <li><a href="/class-schedule/">Class Schedule</a></li>
 	            </ul>
 	            <ul>
-	                <li><a href="index.php?page=facilities">Service &amp; Facilities</a></li>
-	                <li><a href="index.php?page=tanning">Metallic Sol Tanning</a></li>
-	                <li><a href="index.php?page=pit">The Training Pit</a></li>
+	                <li><a href="/service-facilities/">Service &amp; Facilities</a></li>
+	                <li><a href="/metallic-sol-tanning/">Metallic Sol Tanning</a></li>
+	                <li><a href="/training-pit/">The Training Pit</a></li>
 	            </ul>
 	            <ul>
-	                <li><a href="index.php?page=contact">Contact Us</a></li>
+	                <li><a href="/contact-us/">Contact Us</a></li>
 	            </ul>
 	        </div><!-- #menu2 -->
 
